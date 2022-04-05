364f17b553c49a2c9aed25f083c7c5d82ddab926
ValN4597 <101672121+ValN4597@users.noreply.github.com>
2022-04-05 14:54:14 -0700

    Add files via upload

---
+++ js-demo.js
@@ -0,0 +1,28 @@
+﻿/*
+   JS Activity Clock
+   Name: Valerie Navarrete
+   Date: April 5th, 2022
+
+   function getWeekday(dayNum)
+      Returns the text of the day of the week where dayNum
+      is the number of the week from 0 (Sunday) to 6 (Saturday)
+*/
+runClock();
+setInterval("runClock()", 1000);
+
+function runClock(){
+   var thisDay= new Date();
+   var thisDate= thisDay.toLocaleDateString();
+   var thisDayNum= thisDay.getDay();
+   var thisWeekday= getweekday(thisDayNum);
+   var thisTime= thisDay.toLocaleTimeString();
+
+   document.getElementById("date").textContent = thisDate;
+   document.getElementById("wday").textContent = thisWeekday;
+   document.getElementById("time").textContent = thisTime;
+}
+
+function getweekday(dayNum){
+   var wDays = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+   return wDays[dayNum];
+}
